0b34f0e102a82277239abe77551c5b992bbeb858
xXcoronaXx <acevedo.corona@gmail.com>
2015-10-03 20:02:05 +0200

    modelo container

--- DataContainer/models.py
+++ DataContainer/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class Container(models.Model):
+	id_container    = models.CharField(max_length=255)
+	text     = models.TextField()
+	created  = models.DateTimeField(auto_now_add=True)
+	modified = models.DateTimeField(auto_now=True)
